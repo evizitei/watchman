6688847b3133187abf103c0e5c2f543f290aa773
evizitei <ethan.vizitei@gmail.com>
2010-12-19 12:59:40 -0600

    count medic units
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- lib/watchman/medic_watcher.rb
+++ lib/watchman/medic_watcher.rb
@@ -16,10 +16,20 @@ module Watchman
       @_medic_page
     end
     
+    def city_status
+      count_available(/TruckID=M(11|15|21|22|23)\d/)
+    end
+    
     private
     
     def cookie
       Mechanize::Cookie.new("TScreen","4")
+    end
+    
+    def count_available(regex)
+      nodes = page.parser.css("a.AVL")
+      nodes = nodes.map{|node| node.attributes["href"].value}.select{|val| val =~ regex}
+      nodes.size
     end
   end
 end
--- spec/watchman/medic_watcher_spec.rb
+++ spec/watchman/medic_watcher_spec.rb
@@ -9,10 +9,17 @@ describe "Watchman::MedicWatcher" do
     IstatusMocker.fake_medic_path(Client.host)
   end
   
+  let(:watcher){ MedicWatcher.new}
+  
   describe "fetching the medic page" do
     it "can pull the medic apparatus page" do
-      watcher = MedicWatcher.new
       watcher.raw_page.should =~ /M131/
+    end
+  end
+  
+  describe "city status" do
+    it "can count the medic units in the city" do
+      watcher.city_status.should == 4
     end
   end
 end
